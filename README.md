93dc0a27360f9d25ff000bdf7325538989999b2b
evaczeydapommersheim <epommersheim@yahoo.co.uk>
2022-01-31 00:01:42 +0000

    Fisrt commit of week02

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # pands-Problem-Sheet
+---
+## Week02 Problem Sheet
+---
+### **Body Mass Index** - *BMI*
+---
+This program calculates the BMI after inputting variables for *Height* and *Weight*.
+---
+*Author: Eva Czeyda-Pommersheim*
---
+++ bmi.py
@@ -0,0 +1,7 @@
+#Solution for Weekly Task - Week 02
+#Author: Eva Czeyda-Pommersheim
+
+weight = int(input("Please enter your weight (kg): "))
+height = int(input("Please enter your height (cm): "))
+bmi = float(weight / (height / 100))
+print("Your BMI is: {}".format(bmi))
