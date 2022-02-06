59a825b9c27796b88822bc57a9c21eeb59370772
evaczeydapommersheim <epommersheim@yahoo.co.uk>
2022-02-06 15:54:17 +0000

    Week 03_First Commit

--- README.md
+++ README.md
@@ -6,4 +6,11 @@
 ---
 This program calculates the BMI after inputting variables for *Height* and *Weight*.
 ---
+## Week03 Weekly Task
+---
+### **Reverse sentence outputting every second letter**
+---
+This program reads in a sentence and outputs every second letter of the sentence in a reversed manner (starting from the end of sentence)
+Assumption: spaces are considered a character also, therefore are taken into account when couting the letters.
+---
 *Author: Eva Czeyda-Pommersheim*
---
+++ secondstring.py
@@ -0,0 +1,11 @@
+# This is my solution for Week03 Weekly task
+# This program is to read in a sentence
+# Output every second letter of the sentence in a reversed order
+
+# Author: Eva Czeyda-Pommersheim
+
+sentence = input("Please enter a sentence: ")
+print(sentence[::-2])  
+
+# used the slicing of a string based on https://docs.python.org/3/library/functions.html#slice and 
+# googling "read every second letter python code" and reviewing result:https://stackoverflow.com/questions/36003289/write-programs-that-read-a-line-of-input-as-a-string-and-print-every-second-lett/37201212
