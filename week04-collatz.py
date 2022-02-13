c9dbafdbaafa1aa114a67661a0f0fb38b66c1648
evaczeydapommersheim <epommersheim@yahoo.co.uk>
2022-02-13 14:32:55 +0000

    week04 weekly task update: added comments and additional if condition

--- week04-collatz.py
+++ week04-collatz.py
@@ -9,12 +9,16 @@
 # Author: Eva Czeyda-Pommersheim
 
 num = int(input('Please enter a positiv integer: '))
+
 print(num)              # prints out the starting number
-while num != 1:
-    if num % 2 == 0:    # if the number is even there will be no remainder if devided by 2.
+while num != 1:         # WHILE LOOP WILL CONTINUE UNTIL THE OUTCOME IS EQUAL TO 1.
+    if num <= 0:        # The program prompts to only enter numbers greater than zero.
+        print('Please enter a number greater than 0.')
+        num = int(input('Please enter a positiv integer: '))
+    elif num % 2 == 0:    # NUMBERS DIVIDED BY 2 WITH NO REMAINDER ARE EVEN NUMBERS
         num = num / 2
-        print(int(num))
+        print(int(num))     # Print the result, while loop will continue to do the calculation.
     else:
-        num = (num * 3) + 1
-        print(int(num))             #  if the number is not even, the if condition is False it can only be odd
+        num = (num * 3) + 1     #  if the number is not even, the if condition is False AND PROGRAM WILL EXECUTE THE ELSE CONDITION
+        print(int(num))      # Print the result, while loop will continue to do the calculation.       
        
