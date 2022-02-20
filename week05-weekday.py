6ede75eca7e02f536975d773bd0184c6e07863f2
evaczeydapommersheim <epommersheim@yahoo.co.uk>
2022-02-20 23:25:40 +0000

    Week05 task second commit

--- week05-weekday.py
+++ week05-weekday.py
@@ -1,3 +1,28 @@
-# This program outputs whether or not today is a weekday or not.
+# This program outputs whether or not today is a weekday.
 
-#Author: Eva Czeyda-Pommersheim
+# Author: Eva Czeyda-Pommersheim
+
+import datetime # Python has a module called datetime Source: W3 Schools, Python Dates Chapter
+
+today = datetime.datetime.now()
+print(today.strftime("%A"))     # Source: W3 Schools, Python Dates Chapter; this prints the date of the week based on today's date with a type str
+
+days = {
+    "weekday" : [
+        'Monday',
+        'Tuesday',
+        'Wednesday',
+        'Thursday',
+        'Friday',
+        ],
+    "weekend" : [
+        'Saturday',
+        'Sunday'
+    ]
+   }
+
+for day in days:
+    if today == days.itemsweekday.value[:]
+        print("Weekday")
+    
+print("Weekend")
