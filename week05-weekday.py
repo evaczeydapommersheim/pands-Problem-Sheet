cc8de726417aeadda4db91cdc166de51427a3666
evaczeydapommersheim <epommersheim@yahoo.co.uk>
2022-02-27 13:21:02 +0000

    updating week05 weekday.py solution

--- week05-weekday.py
+++ week05-weekday.py
@@ -2,19 +2,19 @@
 
 # Author: Eva Czeyda-Pommersheim
 
-from ast import Break
-from calendar import weekday
 import datetime # Python has a module called datetime Source: W3 Schools, Python Dates Chapter
 
 today = (datetime.datetime.now()).strftime("%A")    # Source: W3 Schools, Python Dates Chapter; this prints the date of the week based on today's date with a type str
 
-weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']
+weekdays = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'] #listing the days during the week
+weekendDays = ['Saturday', 'Sunday'] #created additional variable to list the days of the weekend also
 
-if True:
+if today in weekdays:   #using the if/else function to print out the different outcomes.
     today == any(weekdays[:])  
-            # Using the any() Python built-in function (Source RealPython newsletter https://realpython.com/courses/python-any-boolean-function/?__s=vn9uk2a450mzn2xle474) 
+        # Using the any() Python built-in function (Source RealPython newsletter https://realpython.com/courses/python-any-boolean-function/?__s=vn9uk2a450mzn2xle474) 
         # to iterate through the list of weekdays Mon-Friday. If this condition becomes false, today variable can only be one of the weekend days.
         # additional reading on W3 School https://www.w3schools.com/python/ref_func_any.asp)
     print("Yes, unfortunately today is a weekday.")
 else:
+    today == any(weekendDays[:])
     print("It is the weekend, yay!") 
